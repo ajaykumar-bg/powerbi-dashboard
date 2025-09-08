010fad5ed46db4a2f29b9689bbdad5858479193a
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-09-08 18:19:04 +0530

    Updated layout

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 				}}
 			>
 				<Navbar />
-				<Container maxWidth sx={{ py: 4 }}>
+				<Container maxWidth={false} sx={{ py: 4, px: { xs: 2, sm: 3, md: 4 } }}>
 					<Dashboard />
 				</Container>
 			</Box>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,51 +11,51 @@ import { ServiceRequestSection } from './ServiceRequestSection';
 import { ProductRoadmapSection } from './ProductRoadmapSection';
 
 function Dashboard() {
-  return (
-    <Grid container spacing={3}>
-      {/* Left Side - Three Small Widgets */}
-      <Grid item xs={12} md={3}>
-        <Grid container spacing={2} direction="column">
-          <Grid item xs={12}>
-            <TechDebtSection />
-          </Grid>
-          <Grid item xs={12}>
-            <AppRatWidget />
-          </Grid>
-          <Grid item xs={12}>
-            <AIIndexWidget />
-          </Grid>
-        </Grid>
-      </Grid>
+	return (
+		<Grid container spacing={3}>
+			{/* Left Side - Three Small Widgets */}
+			<Grid item xs={12} md={2}>
+				<Grid container spacing={2} direction='column'>
+					<Grid item xs={12}>
+						<TechDebtSection />
+					</Grid>
+					<Grid item xs={12}>
+						<AppRatWidget />
+					</Grid>
+					<Grid item xs={12}>
+						<AIIndexWidget />
+					</Grid>
+				</Grid>
+			</Grid>
 
-      {/* Middle Side - Large Sections */}
-      <Grid item xs={12} md={6}>
-        <Grid container spacing={3} direction="column">
-          <Grid item xs={12}>
-            <VulnerabilitiesSection />
-          </Grid>
-          <Grid item xs={12}>
-            <SQLOptimizationSection />
-          </Grid>
-          <Grid item xs={12}>
-            <ProductRoadmapSection />
-          </Grid>
-        </Grid>
-      </Grid>
+			{/* Middle Side - Large Sections */}
+			<Grid item xs={12} md={7}>
+				<Grid container spacing={3} direction='column'>
+					<Grid item xs={12}>
+						<VulnerabilitiesSection />
+					</Grid>
+					<Grid item xs={12}>
+						<SQLOptimizationSection />
+					</Grid>
+					<Grid item xs={12}>
+						<ProductRoadmapSection />
+					</Grid>
+				</Grid>
+			</Grid>
 
-      {/* Right Side */}
-      <Grid item xs={12} md={3}>
-        <Grid container spacing={3} direction="column">
-          <Grid item>
-            <OperationsMetricsSection />
-          </Grid>
-          <Grid item>
-            <ServiceRequestSection />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
+			{/* Right Side */}
+			<Grid item xs={12} md={3}>
+				<Grid container spacing={3} direction='column'>
+					<Grid item xs={12}>
+						<OperationsMetricsSection />
+					</Grid>
+					<Grid item xs={12}>
+						<ServiceRequestSection />
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default Dashboard;

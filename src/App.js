cada660b638582ae65711b48fd6310f5d2a70653
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-09-08 14:53:36 +0530

    Added Dashboard component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import {
   ThemeProvider,
   CssBaseline,
   Container,
-  Grid,
   Typography,
   Box,
 } from '@mui/material';
 import { theme } from './theme';
-import { TechDebtSection } from './components/TechDebtSection';
-import { AppRatWidget } from './components/AppRatWidget';
-import { AIIndexWidget } from './components/AIIndexWidget';
-import { VulnerabilitiesSection } from './components/VulnerabilitiesSection';
-import { SQLOptimizationSection } from './components/SQLOptimizationSection';
-import { OperationsMetricsSection } from './components/OperationsMetricsSection';
-import { ServiceRequestSection } from './components/ServiceRequestSection';
-import { ProductRoadmapSection } from './components/ProductRoadmapSection';
+
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -32,50 +25,7 @@ function App() {
           <Typography variant="h4" gutterBottom>
             SAP App Dev Scorecard
           </Typography>
-
-          <Grid container spacing={3}>
-            {/* Left Side - Three Small Widgets */}
-            <Grid item xs={12} md={3}>
-              <Grid container spacing={2} direction="column">
-                <Grid item xs={12}>
-                  <TechDebtSection />
-                </Grid>
-                <Grid item xs={12}>
-                  <AppRatWidget />
-                </Grid>
-                <Grid item xs={12}>
-                  <AIIndexWidget />
-                </Grid>
-              </Grid>
-            </Grid>
-
-            {/* Middle Side - Large Sections */}
-            <Grid item xs={12} md={6}>
-              <Grid container spacing={3} direction="column">
-                <Grid item xs={12}>
-                  <VulnerabilitiesSection />
-                </Grid>
-                <Grid item xs={12}>
-                  <SQLOptimizationSection />
-                </Grid>
-                <Grid item xs={12}>
-                  <ProductRoadmapSection />
-                </Grid>
-              </Grid>
-            </Grid>
-
-            {/* Right Side */}
-            <Grid item xs={12} md={3}>
-              <Grid container spacing={3} direction="column">
-                <Grid item>
-                  <OperationsMetricsSection />
-                </Grid>
-                <Grid item>
-                  <ServiceRequestSection />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
+          <Dashboard />
         </Container>
       </Box>
     </ThemeProvider>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+import { TechDebtSection } from './TechDebtSection';
+import { AppRatWidget } from './AppRatWidget';
+import { AIIndexWidget } from './AIIndexWidget';
+import { VulnerabilitiesSection } from './VulnerabilitiesSection';
+import { SQLOptimizationSection } from './SQLOptimizationSection';
+import { OperationsMetricsSection } from './OperationsMetricsSection';
+import { ServiceRequestSection } from './ServiceRequestSection';
+import { ProductRoadmapSection } from './ProductRoadmapSection';
+
+function Dashboard() {
+  return (
+    <Grid container spacing={3}>
+      {/* Left Side - Three Small Widgets */}
+      <Grid item xs={12} md={3}>
+        <Grid container spacing={2} direction="column">
+          <Grid item xs={12}>
+            <TechDebtSection />
+          </Grid>
+          <Grid item xs={12}>
+            <AppRatWidget />
+          </Grid>
+          <Grid item xs={12}>
+            <AIIndexWidget />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Middle Side - Large Sections */}
+      <Grid item xs={12} md={6}>
+        <Grid container spacing={3} direction="column">
+          <Grid item xs={12}>
+            <VulnerabilitiesSection />
+          </Grid>
+          <Grid item xs={12}>
+            <SQLOptimizationSection />
+          </Grid>
+          <Grid item xs={12}>
+            <ProductRoadmapSection />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Right Side */}
+      <Grid item xs={12} md={3}>
+        <Grid container spacing={3} direction="column">
+          <Grid item>
+            <OperationsMetricsSection />
+          </Grid>
+          <Grid item>
+            <ServiceRequestSection />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Dashboard;

a2ef4be8ed9e106d93e7da6bf7b8ea972a922a40
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-09-09 14:17:23 +0530

    Prettier fix

--- src/components/ProductRoadmapSection.js
+++ src/components/ProductRoadmapSection.js
@@ -2,29 +2,29 @@ import { Paper, Typography, Grid } from '@mui/material';
 import { useDashboard } from '../context/DashboardContext';
 
 export const ProductRoadmapSection = () => {
-	const { data } = useDashboard();
+  const { data } = useDashboard();
 
-	const { productRoadmap } = data;
+  const { productRoadmap } = data;
 
-	return (
-		<Paper sx={{ p: 2, height: '100%' }}>
-			<Typography variant='h6' gutterBottom>
-				Product EOL/Roadmap
-			</Typography>
+  return (
+    <Paper sx={{ p: 2, height: '100%' }}>
+      <Typography variant="h6" gutterBottom>
+        Product EOL/Roadmap
+      </Typography>
 
-			<Grid container spacing={2} justifyContent={'space-between'}>
-				{productRoadmap?.items?.map((item, index) => (
-					<Grid item xs={2} key={index}>
-						<Typography variant='body1'>{item.name}</Typography>
-						<Typography variant='h6' color='warning.main'>
-							{item.year}
-						</Typography>
-						<Typography variant='body2' color='success.main'>
-							{item.type}
-						</Typography>
-					</Grid>
-				))}
-			</Grid>
-		</Paper>
-	);
+      <Grid container spacing={2} justifyContent={'space-between'}>
+        {productRoadmap?.items?.map((item, index) => (
+          <Grid item xs={2} key={index}>
+            <Typography variant="body1">{item.name}</Typography>
+            <Typography variant="h6" color="warning.main">
+              {item.year}
+            </Typography>
+            <Typography variant="body2" color="success.main">
+              {item.type}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
+  );
 };

7886f5815438739ef2e7a1a68b6a90730cb62472
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-09-10 20:12:30 +0530

    Updated colors

--- src/components/ServiceRequestSection.js
+++ src/components/ServiceRequestSection.js
@@ -7,81 +7,81 @@ import { useMemo } from 'react';
 import { generateServiceRequestData } from '../utils/dataGenerator';
 
 export const ServiceRequestSection = () => {
-  const { data } = useDashboard();
+	const { data } = useDashboard();
 
-  const { serviceNowRequest } = data;
+	const { serviceNowRequest } = data;
 
-  const serviceNowData = useMemo(
-    () => generateServiceRequestData(serviceNowRequest),
-    [serviceNowRequest]
-  );
-  return (
-    <Paper sx={{ p: 2, height: '100%' }}>
-      <Typography variant="h6" gutterBottom>
-        Service Now Request
-      </Typography>
-      <Box>
-        <PieChart
-          series={[
-            {
-              data: serviceNowData,
-            },
-          ]}
-          width={200}
-          height={200}
-        />
-      </Box>
+	const serviceNowData = useMemo(
+		() => generateServiceRequestData(serviceNowRequest),
+		[serviceNowRequest]
+	);
+	return (
+		<Paper sx={{ p: 2, height: '100%' }}>
+			<Typography variant='h6' gutterBottom>
+				Service Now Request
+			</Typography>
+			<Box>
+				<PieChart
+					series={[
+						{
+							data: serviceNowData,
+						},
+					]}
+					width={200}
+					height={200}
+				/>
+			</Box>
 
-      <Grid container spacing={2} justifyContent={'space-between'}>
-        <Grid item xs={4}>
-          <Typography variant="h4" color="primary">
-            {serviceNowRequest?.processed}
-          </Typography>
-          <Typography variant="body2">Processed</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="h4" color="warning">
-            {serviceNowRequest?.inProgress}
-          </Typography>
-          <Typography variant="body2">In-Progress</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="h4" color="success">
-            {serviceNowRequest?.completed}
-          </Typography>
-          <Typography variant="body2">Completed</Typography>
-        </Grid>
-      </Grid>
-      <Divider sx={{ my: 2 }} />
-      <Box
-        sx={{
-          display: 'grid',
-          justifyContent: 'center',
-          textAlign: 'center',
-          mt: 2,
-        }}
-      >
-        <Typography variant="h6" gutterBottom>
-          % Completion
-        </Typography>
-        <Gauge
-          width={150}
-          height={150}
-          value={serviceNowRequest?.completionPercentage}
-          sx={(theme) => ({
-            [`& .${gaugeClasses.valueText}`]: {
-              color: theme.palette.text.secondary,
-              fontSize: 40,
-            },
-            [`& .${gaugeClasses.valueArc}`]: {
-              fill: '#52b202',
-            },
-            [`& .${gaugeClasses.referenceArc}`]: {
-              fill: theme.palette.text.disabled,
-            },
-          })}
-        />
-      </Box>
-    </Paper>
-  );
+			<Grid container spacing={2} justifyContent={'space-between'}>
+				<Grid item xs={4}>
+					<Typography variant='h4' color='primary'>
+						{serviceNowRequest?.processed}
+					</Typography>
+					<Typography variant='body2'>Processed</Typography>
+				</Grid>
+				<Grid item xs={4}>
+					<Typography variant='h4' color='warning'>
+						{serviceNowRequest?.inProgress}
+					</Typography>
+					<Typography variant='body2'>In-Progress</Typography>
+				</Grid>
+				<Grid item xs={4}>
+					<Typography variant='h4' color='success'>
+						{serviceNowRequest?.completed}
+					</Typography>
+					<Typography variant='body2'>Completed</Typography>
+				</Grid>
+			</Grid>
+			<Divider sx={{ my: 2 }} />
+			<Box
+				sx={{
+					display: 'grid',
+					justifyContent: 'center',
+					textAlign: 'center',
+					mt: 2,
+				}}
+			>
+				<Typography variant='h6' gutterBottom>
+					% Completion
+				</Typography>
+				<Gauge
+					width={150}
+					height={150}
+					value={serviceNowRequest?.completionPercentage}
+					sx={(theme) => ({
+						[`& .${gaugeClasses.valueText}`]: {
+							color: theme.palette.text.secondary,
+							fontSize: 40,
+						},
+						[`& .${gaugeClasses.valueArc}`]: {
+							fill: theme.palette.success.main,
+						},
+						[`& .${gaugeClasses.referenceArc}`]: {
+							fill: theme.palette.text.disabled,
+						},
+					})}
+				/>
+			</Box>
+		</Paper>
+	);
 };
--- src/components/TechDebtSection.js
+++ src/components/TechDebtSection.js
@@ -3,16 +3,16 @@ import { useDashboard } from '../context/DashboardContext';
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 
 export const TechDebtSection = () => {
-  const { data } = useDashboard();
+	const { data } = useDashboard();
 
-  const { techDebt } = data;
+	const { techDebt } = data;
 
-  return (
-    <Paper sx={{ px: 2, py: 5, height: '100%' }}>
-      <Typography variant="h6" gutterBottom>
-        Tech Debt Reduction
-      </Typography>
-      {/* <Typography
+	return (
+		<Paper sx={{ px: 2, py: 5, height: '100%' }}>
+			<Typography variant='h6' gutterBottom>
+				Tech Debt Reduction
+			</Typography>
+			{/* <Typography
 				variant='h2'
 				sx={{
 					color: 'secondary.main',
@@ -23,32 +23,32 @@ export const TechDebtSection = () => {
 			>
 				{techDebt?.reductionPercentage}%
 			</Typography> */}
-      <Box
-        sx={{
-          display: 'grid',
-          justifyContent: 'center',
-          textAlign: 'center',
-          mt: 2,
-        }}
-      >
-        <Gauge
-          width={150}
-          height={150}
-          value={techDebt?.reductionPercentage}
-          sx={(theme) => ({
-            [`& .${gaugeClasses.valueText}`]: {
-              color: theme.palette.text.secondary,
-              fontSize: 40,
-            },
-            [`& .${gaugeClasses.valueArc}`]: {
-              fill: '#ffa726',
-            },
-            [`& .${gaugeClasses.referenceArc}`]: {
-              fill: theme.palette.text.disabled,
-            },
-          })}
-        />
-      </Box>
-    </Paper>
-  );
+			<Box
+				sx={{
+					display: 'grid',
+					justifyContent: 'center',
+					textAlign: 'center',
+					mt: 2,
+				}}
+			>
+				<Gauge
+					width={150}
+					height={150}
+					value={techDebt?.reductionPercentage}
+					sx={(theme) => ({
+						[`& .${gaugeClasses.valueText}`]: {
+							color: theme.palette.text.secondary,
+							fontSize: 40,
+						},
+						[`& .${gaugeClasses.valueArc}`]: {
+							fill: theme.palette.warning.main,
+						},
+						[`& .${gaugeClasses.referenceArc}`]: {
+							fill: theme.palette.text.disabled,
+						},
+					})}
+				/>
+			</Box>
+		</Paper>
+	);
 };

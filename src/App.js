10a3f6afd2caf942c23857070aa79a54cd0b0523
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-09-15 23:14:10 +0530

    Added routing with Login & Details pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import { Container, CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Details from './components/Details';
 import { DashboardProvider } from './context/DashboardContext';
 import { ThemeProvider } from './context/ThemeContext';
 
 import './App.css';
 
 function App() {
-	return (
-		<ThemeProvider>
-			<DashboardProvider>
-				<CssBaseline />
-				<div className='app'>
-					<Navbar />
-					<Container maxWidth={false} sx={{ padding: 2 }}>
-						<Dashboard />
-					</Container>
-				</div>
-			</DashboardProvider>
-		</ThemeProvider>
-	);
+    return (
+        <ThemeProvider>
+            <DashboardProvider>
+                <CssBaseline />
+                <Router>
+                    <div className='app'>
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/*" element={
+                                <>
+                                    <Navbar />
+                                    <Container maxWidth={false} sx={{ padding: 2 }}>
+                                        <Routes>
+                                            <Route path="/" element={<Dashboard />} />
+                                            <Route path="/details" element={<Details />} />
+                                            <Route path="*" element={<Navigate to="/" replace />} />
+                                        </Routes>
+                                    </Container>
+                                </>
+                            } />
+                        </Routes>
+                    </div>
+                </Router>
+            </DashboardProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;

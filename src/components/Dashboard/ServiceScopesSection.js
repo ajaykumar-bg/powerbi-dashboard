9992bb067f73dc4f4c16128c466d3a73a3908097
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-10-27 12:10:28 +0530

    Updated service scopes with explicitly numbers

--- src/components/Dashboard/ServiceScopesSection.js
+++ src/components/Dashboard/ServiceScopesSection.js
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { Box, Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography, Grid, Divider } from '@mui/material';
 import { useDashboard } from '../../context/DashboardContext';
 
 import { PieChart } from '@mui/x-charts/PieChart';
@@ -34,6 +34,35 @@ export const ServiceScopesSection = () => {
           height={200}
         />
       </Box>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Grid container spacing={2} justifyContent={'space-between'}>
+        <Grid item xs={6}>
+          <Typography variant='h5' sx={{ color: serviceScopesData[0]?.color }}>
+            {serviceScopes?.ricefs?.toLocaleString()}
+          </Typography>
+          <Typography variant='body2'>RICEFs</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h5' sx={{ color: serviceScopesData[1]?.color }}>
+            {serviceScopes?.retrofits?.toLocaleString()}
+          </Typography>
+          <Typography variant='body2'>Retrofits</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h5' sx={{ color: serviceScopesData[2]?.color }}>
+            {serviceScopes?.fioriApps?.toLocaleString()}
+          </Typography>
+          <Typography variant='body2'>Fiori Apps</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h5' sx={{ color: serviceScopesData[3]?.color }}>
+            {serviceScopes?.liveCompare?.count?.toLocaleString()}
+          </Typography>
+          <Typography variant='body2'>Live Compare</Typography>
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
